8689016488a5bca54e7823270d56d587f74850b7
Kadiraydn01 <kadiraydn01@gmail.com>
2023-10-04 00:18:45 +0300

    Devam edilecek

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Homepage from "./components/Homepage";
 import ProductPage from "./ProductPage/ProductPage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Products from "./components/Products";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Homepage} />
         <Route path="/products" component={ProductPage} />
+        <Route path="/product" component={Products} />
       </Switch>
     </Router>
   );
