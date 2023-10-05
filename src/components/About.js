7cdedee1d5e59d08902c9ef48bd1426194117974
Kadiraydn01 <kadiraydn01@gmail.com>
2023-10-05 22:54:45 +0300

    About bitti

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,67 @@ import foto from "../images/about.png";
 import user from "../images/teamUser.jpg";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
 import { AiFillFacebook } from "react-icons/ai";
+import video from "../images/video.mp4";
+import foto1 from "../images/shopping.png";
 const About = () => {
   return (
     <>
       <Semiheader />
-      <div>resimli</div>
-      <div>problem</div>
-      <div>15k</div>
-      <div>video</div>
+      <div className="flex gap-40 px-60 py-20">
+        <div className="flex flex-col gap-9 items-start">
+          <h1 className="text-sm font-bold">ABOUT COMPANY</h1>
+          <h2 className="text-5xl font-bold">ABOUT US</h2>
+          <p className="w-[350px] ">
+            We know how large objects will act, but things on a small scale
+          </p>
+          <button className="px-10 py-3 border bg-blue-400 rounded-xl text-white">
+            Get Quote Now
+          </button>
+        </div>
+        <div>
+          <img className="h-[430px]" src={foto1} alt="foto" />
+        </div>
+      </div>
+      <div className="flex gap-24 items-center justify-center">
+        <div className="flex items-start gap-6 flex-col">
+          <h1 className="text-red-600">Problems trying</h1>
+          <p className="w-[450px] text-xl font-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quod?
+          </p>
+        </div>
+        <div>
+          <p className="w-[500px] flex">
+            Promblems trying to solve them by using the most efficient and
+            effective means possible.
+          </p>
+        </div>
+      </div>
+      <div className="px-96">
+        <div className="flex gap-12 py-20">
+          <div className="flex flex-col gap-4 items-center w-64">
+            <h1 className="font-bold text-5xl">15K</h1>
+            <h2>Happy Customers</h2>
+          </div>
+          <div className="flex flex-col gap-4 items-center w-64">
+            <h1 className="font-bold text-5xl">150K</h1>
+            <h2>Monthly Visitors</h2>
+          </div>
+          <div className="flex flex-col gap-4 items-center w-64">
+            <h1 className="font-bold text-5xl">15</h1>
+            <h2>Countries Worldwide</h2>
+          </div>
+          <div className="flex flex-col gap-4 items-center w-64">
+            <h1 className="font-bold text-5xl">100+</h1>
+            <h2>Top Partners</h2>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center py-6 px-14">
+        <video width="1000" height="450" src={video} controls />
+      </div>
 
-      <div className="flex flex-col px-52 py-36 gap-20">
+      <div className="flex flex-col px-52 py-24 gap-20">
         <div className="flex text-4xl font-bold items-center justify-center">
           <h1>Meet Our Team</h1>
         </div>
